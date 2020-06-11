8a54e00421e67f2dc0ea6941a07e6942d0a9d393
Karl Matuke <karl.matuke@qccareerschool.com>
2020-06-11 15:16:28 -0400

    added cors

--- app/index.ts
+++ app/index.ts
@@ -28,6 +28,7 @@ mongoose.connect(uri || connString, {
 // app.use(express.static(path.join(__dirname, 'build')));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
