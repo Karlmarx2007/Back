6edf9b74bb8fb10987e6e5fe95b47aefc125ee5d
Karl Matuke <karl.matuke@qccareerschool.com>
2020-06-11 15:44:46 -0400

    added cors for patch

--- app/index.ts
+++ app/index.ts
@@ -29,6 +29,7 @@ mongoose.connect(uri || connString, {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH');
   next();
 });
 
